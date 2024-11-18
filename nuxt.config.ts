10d5bb5f229d5eac1179ed1a6e09e3976b854ed8
hasbi <sinhasbi123@gmail.com>
2024-11-18 16:59:20 +0800

    Nuxt3 test2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,38 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  // 元件自動導入
+  // ssr: false,
+  // nitro: {
+  //   prerender: {
+  //     routes: ['/'], // 預渲染的路由
+  //   },
+  // },
   components: true,
   // vue-tsc 之後使用 dev 會自動檢查型別
   typescript: {
     strict: true,
     typeCheck: true,
   },
-  modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@element-plus/nuxt",
+  ],
+  // 選擇要引入甚麼東西
+  elementPlus: {
+    // importStyle: 'css',
+    // components: ['ElButton', 'ElInput'],
+  },
+
+  css: ["@/assets/styles/main.scss"],
+  // vite 目前不支持 legacy 的 API
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+      },
+    },
+  },
 });
